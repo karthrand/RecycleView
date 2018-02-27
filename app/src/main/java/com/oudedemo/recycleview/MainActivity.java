ce836b8fead3449b36e3f4e0bea0876994242591
karthrand <735867550@qq.com>
2018-02-27 12:23:56 +0800

    Recycle简单实现

--- app/src/main/java/com/oudedemo/recycleview/MainActivity.java
+++ app/src/main/java/com/oudedemo/recycleview/MainActivity.java
@@ -2,13 +2,43 @@ package com.oudedemo.recycleview;
 
 import android.app.*;
 import android.os.*;
+import android.support.v7.widget.*;
+import java.util.*;
 
 public class MainActivity extends Activity 
 {
+    private List<MyList> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initMylist();
+        RecyclerView recycleView = (RecyclerView) findViewById(R.id.mainRecyclerView1);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recycleView.setLayoutManager(layoutManager);
+        MyListAdapter adapter =new MyListAdapter(list);
+        recycleView.setAdapter(adapter);
     }
+    
+    
+    private void initMylist()
+    {
+        for (int i=0;i<2;i++){
+            MyList  list1 = new MyList("List1",R.drawable.ic_launcher);
+            list.add(list1);
+            MyList  list2 = new MyList("List2",R.drawable.ic_launcher);
+            list.add(list2);
+            MyList  list3 = new MyList("List3",R.drawable.ic_launcher);
+            list.add(list3);
+            MyList  list4 = new MyList("List4",R.drawable.ic_launcher);            list.add(list4);
+            MyList  list5 = new MyList("List5",R.drawable.ic_launcher);
+            list.add(list5);
+            MyList  list6 = new MyList("List6",R.drawable.ic_launcher);
+            list.add(list6);
+            MyList  list7 = new MyList("List7",R.drawable.ic_launcher);
+            list.add(list7);
+        }
+    }
+    
 }
---
+++ app/src/main/java/com/oudedemo/recycleview/MyList.java
@@ -0,0 +1,18 @@
+package com.oudedemo.recycleview;
+
+
+public class MyList
+{
+    private String name;
+    private int imageId;
+    public MyList(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
+}
